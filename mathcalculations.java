6c1fd7c79b1015f8fb1e1e37dc6b8daee38e7e07
nivasjava <43057337+nivasjava@users.noreply.github.com>
2018-09-07 11:06:37 +0530

    Add files via upload

---
+++ mathcalculations.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args)
+   {
+    int a;
+    float b,c;
+    a=25;b=25;
+    c=a+b;
+    System.out.println("multiplication of 2 integer"+c);
+  }
+}
