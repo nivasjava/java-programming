8c6c1074e5ad24ad57b2266df6a11d73d7b053f6
nivasjava <43057337+nivasjava@users.noreply.github.com>
2018-09-07 15:29:15 +0530

    Add files via upload

---
+++ new scanner add.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner nivas=new Scanner(System.in);
+    System.out.println("enter the first number:");
+    int a=nivas.nextInt();
+    System.out.println("enter the second number:");
+    int b=nivas.nextInt();
+    System.out.println("enter the third number:");
+    int c=nivas.nextInt();
+    System.out.println("enter the fourth number:");
+    int d=nivas.nextInt();
+    System.out.println("enter the fiveth number:");
+    int e=nivas.nextInt();
+    int f=a+b+c+d+e;
+    System.out.println(f);
+    int g=a-b-c-d-e;
+    System.out.println(g);
+    int h=a*b*c*d*e;
+    System.out.println(h);
+}
+}
