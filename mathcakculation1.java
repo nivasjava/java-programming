3088379c6adc2e917115ba11c32a0220da80264e
nivasjava <43057337+nivasjava@users.noreply.github.com>
2018-09-07 12:32:01 +0530

    Add files via upload

---
+++ mathcakculation1.java
@@ -0,0 +1,15 @@
+class Main  {
+  public static void main(String[] args)
+   {
+     int a,b,f;
+     float c,d,e;
+    a=25;b=10;
+    c=20.5f;
+     d=a+b;
+     e=a+c;
+     f=a*b;
+     System.out.println("addition of 2 integer"+d);
+     System.out.println("addition of integer and float"+e);
+     System.out.println("multiplication  of 2 integer"+f);
+   }
+}
